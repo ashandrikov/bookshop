9fa6cc7c6b8b48cf8ecda906a82b816e346076e7
ashandrikov <ashandrikov@gmail.com>
2023-02-22 12:01:49 +0200

    Divide filter chain into two: one for working with rest (postman) and other is for web requests

--- src/main/java/com/shandrikov/bookshop/configuration/SecurityConfiguration.java
+++ src/main/java/com/shandrikov/bookshop/configuration/SecurityConfiguration.java
@@ -1,18 +1,24 @@
 package com.shandrikov.bookshop.configuration;
 
-import com.shandrikov.bookshop.enums.Role;
 import com.shandrikov.bookshop.exceptions.FilterChainExceptionHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
 
 @Configuration
 @EnableWebSecurity
@@ -22,41 +28,50 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
     private final FilterChainExceptionHandler filterChainExceptionHandler;
 
+    @Order(HIGHEST_PRECEDENCE)
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeHttpRequests()
-                    .requestMatchers("/css/*", "/js/*").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/restapi/auth/**").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/restapi/book").hasAuthority(Role.EDITOR.toString())
-                    .requestMatchers("/restapi/admin/**").hasAuthority(Role.ADMINISTRATOR.toString())
-                    .requestMatchers("/restapi/cart/**", "/restapi/user/**").hasAuthority(Role.USER.toString())
-                    .anyRequest().authenticated()
-//                .and()
-//                    .formLogin()
-//                    .loginPage("/login").permitAll()
-//                    .defaultSuccessUrl("/api/books", true)
-//                .and()
-//                    .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-//                    .key("R24FfUSP7Y6c1#a")
-//                .and()
-//                    .logout()
-//                    .logoutUrl("/logout")
-//                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-//                    .clearAuthentication(true)
-//                    .invalidateHttpSession(true)
-//                    .deleteCookies("JSESSIONID", "remember-me")
-//                    .logoutSuccessUrl("/login")
-                .and()
-                    .httpBasic()
-                .and()
-                    .authenticationProvider(authenticationProvider)
-                    .addFilterBefore(filterChainExceptionHandler, LogoutFilter.class)
-                    .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+    public SecurityFilterChain jwtFilterChain(HttpSecurity http) throws Exception {
+        return http.securityMatcher("/restapi/**")
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
+
+    @Bean
+    public SecurityFilterChain webFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/css/*", "/js/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/api/books", true)
+                .and()
+                    .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                    .key("R24FfUSP7Y6c1#a")
+                .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login")
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(filterChainExceptionHandler, LogoutFilter.class)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+//             TODO: set request matchers for web
+//            .requestMatchers(HttpMethod.POST, "/restapi/auth/**").permitAll()
+//            .requestMatchers(HttpMethod.POST, "/restapi/book").hasAuthority(Role.EDITOR.toString())
+//            .requestMatchers("/restapi/admin/**").hasAuthority(Role.ADMINISTRATOR.toString())
+//            .requestMatchers("/restapi/cart/**", "/restapi/user/**").hasAuthority(Role.USER.toString())
+//    }
 }
